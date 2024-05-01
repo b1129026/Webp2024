271b236198c16b26e12790e7dc4a55b645c93782
b1129026 <119938471+b1129026@users.noreply.github.com>
2024-05-01 16:57:14 +0800

    Add files via upload

---
+++ Lab4/react_login/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CGU_Login from'./cgu_login';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {CGU_Login() }
+      </div>
+    </div>
+  );
+}
+
+export default App;
